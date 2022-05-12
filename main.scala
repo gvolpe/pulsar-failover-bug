d37c44e61d94a484ead5354890d77bd45f466b60
Gabriel Volpe <volpegabriel@gmail.com>
2022-05-12 14:53:22 +0200

    step 3: add key-shared consumer, not yet reproducible

--- main.scala
+++ main.scala
@@ -35,23 +35,31 @@ object Main extends IOApp.Simple:
       .withShardKey(s => ShardKey.Of(s.hashCode.toString.getBytes(UTF_8)))
       .withDeduplication(SeqIdMaker.fromEq[String])
 
+  val sharedSub =
+    Subscription.Builder
+      .withName("buggy-ks")
+      .withType(Subscription.Type.KeyShared)
+      .build
+
   val resources =
     for
       pulsar <- Pulsar.make[IO](config.url)
       c1 <- Consumer.make[IO, String](pulsar, topic, subs, decoder, handler)
       c2 <- Consumer.make[IO, String](pulsar, topic, subs, decoder, handler)
-      p1 <- Producer.make[IO, String](pulsar, topic, encoder)
-    yield (c1, c2, p1)
+      c3 <- Consumer.make[IO, String](pulsar, topic, sharedSub, decoder, handler)
+      p1 <- Producer.make[IO, String](pulsar, topic, encoder, pSettings)
+    yield (c1, c2, c3, p1)
 
   def run: IO[Unit] =
     Stream
       .resource(resources)
-      .flatMap { (c1, c2, p1) =>
+      .flatMap { (c1, c2, c3, p1) =>
         Stream(
+          c3.autoSubscribe.evalMap(s => IO.println(s"C3: $s")),
           c1.autoSubscribe.evalMap(s => IO.println(s"C1: $s")),
           c2.autoSubscribe.evalMap(s => IO.println(s"C2: $s")),
           Stream.emit("foo").covary[IO].metered(1.second).evalMap(p1.send_)
-        ).parJoin(3)
+        ).parJoin(4)
       }
       .interruptAfter(5.seconds)
       .compile
